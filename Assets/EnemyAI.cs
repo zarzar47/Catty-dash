665fb6b2f20a834913c2cc9fd1ac071b15f0c280
Saad-Imam <saadlibra10@gmail.com>
2024-07-16 15:03:03 +0500

    Null error in Enemy AI fixed

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -19,17 +19,19 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 difference = player.transform.position - this.transform.position;
+        if (player!=null){
+            Vector3 difference = player.transform.position - this.transform.position;
 
-        float dot = difference.x * this.transform.position.x + difference.z * this.transform.position.z;
-        if (dot < 0) return;
+            float dot = difference.x * this.transform.position.x + difference.z * this.transform.position.z;
+            if (dot < 0) return;
 
-        this.transform.LookAt(player.transform.position);
+            this.transform.LookAt(player.transform.position);
 
-        if (difference.sqrMagnitude > Mathf.Pow(maxDistance, 2)){
-            this.transform.position += new Vector3(this.transform.forward.x, 0, this.transform.forward.z) * speed;
-            //Debug.DrawRay(this.transform.position, difference, Color.blue, 5);
-            Debug.DrawRay(this.transform.position, this.transform.forward * 3, Color.green, 5);
+            if (difference.sqrMagnitude > Mathf.Pow(maxDistance, 2)){
+                this.transform.position += new Vector3(this.transform.forward.x, 0, this.transform.forward.z) * speed;
+                //Debug.DrawRay(this.transform.position, difference, Color.blue, 5);
+                Debug.DrawRay(this.transform.position, this.transform.forward * 3, Color.green, 5);
+            }
         }
         
     }
